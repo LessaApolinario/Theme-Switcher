c71f78d37258fa57feca26d4b22691294cf653bb
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-08 18:05:29 -0300

    Add transitions to the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,21 @@
 const toggleButton = document.querySelector('#toggle-button');
+const container = document.querySelector('.container');
 
 toggleButton.addEventListener('click', () => {
   const hasDarkClass = toggleButton.classList.contains('dark');
+  const isContainerDark = container.classList.contains('dark');
 
-  if (hasDarkClass) {
+  if (hasDarkClass && isContainerDark) {
     toggleButton.classList.remove('dark');
     toggleButton.classList.add('light');
+
+    container.classList.remove('dark');
+    container.classList.add('light');
   } else {
     toggleButton.classList.remove('light');
     toggleButton.classList.add('dark');
+
+    container.classList.remove('light');
+    container.classList.add('dark');
   }
 });
